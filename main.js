1479d33eae1d3d89799d022514837fac70b61432
SravaniBhumireddy <sravanivengalareddy2004@gmail.com>
2025-12-02 22:42:34 +0530

    Made Udates

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+console.log(window);
+alert(1);
+*/
+
+//single element 
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('.container'));//It shows the whole section form
+//console.log(document.querySelector('h1'));//It shows what is in h1 tag
+
+
+//multiple elements
+
+console.log(document.querySelectorAll('.item'));//NodeList(3) [li.item, li.item, li.item]
+console.log(document.getElementsByClassName('item'));
