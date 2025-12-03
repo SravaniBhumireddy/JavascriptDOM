3d8758634c8e2821e32d9c6da68bd66483dff3f6
SravaniBhumireddy <sravanivengalareddy2004@gmail.com>
2025-12-03 19:54:33 +0530

    Made Updates

--- main.js
+++ main.js
@@ -18,10 +18,21 @@ alert(1);
 const items= document.querySelectorAll('.item');
 
 items.forEach((item) => console.log(item));
-*/
 
 const ul= document.querySelector('.items');
 
 //ul.remove();// deletes entire array
 //ul.lastElementChild.remove();//deletes the last element in array
+
 ul.firstElementChild.textContent = 'Hello';//changes the first element item 1 as Hello
+ul.children[1].innerText='brad';//It changes the value item 2 to brad
+ul.lastElementChild.innerHTML='<h1>World</h1>';//it changes values item 3 to Hello
+*/
+
+const btn=document.querySelector('.btn');
+//btn.style.background='red';//It changes the color of the btn from black to red
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e);
+});
